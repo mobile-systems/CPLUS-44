78b2e80f14d6f835cc1da7fc1a75c1900f0f7a13
Alexander Ocheretin <mobidev@yandex.ru>
2024-05-30 16:42:47 +0300

    Задание 7.2.2

--- CPLUS-44-7.2.2/main.cpp
+++ CPLUS-44-7.2.2/main.cpp
@@ -7,36 +7,51 @@
 Перепишите функцию вычисления гармонического среднего, так чтобы функция возвращала 
 булевское значение, а результат записывался в аргумент типа float — hmean, 
 переданный в функцию по ссылке. В случае неудачи значение hmean должно быть 0.0
-
-Дополните данную функцию ассертами, которые покроют нештатные или нелогичные ситуации
- (нулевой массив, элемент массива равен нулю, сумма элементов массива равна нулю), 
- дополните ассерты текстом ошибки. Вспомните, что вещественные числа мы не можем 
- сравнивать напрямую с нулем, а сравниваем их с погрешностью — эпсилоном, 
- в нашем примере возьмем число 0.0001 за эпсилон.
 */
-bool harmonic_mean(float arr[], int size, float& result)
+bool harmonic_mean(float arr[], int size, float& hmean)
 {
-    if (size) 
-    //(size && "Array size 0!");
+    if (!size)  //(size && "Array size 0!");
+    {
+        hmean = 0;
+        return false;
+    }
+
     float sum = 0;
     for (int i = 0; i < size; i++)
     {
-        assert( (arr[i] >  0.0001 || arr[i] < -0.0001) && "Array element 0!");
+        //assert( (arr[i] >  0.0001 || arr[i] < -0.0001) && "Array element 0!");
+        if (!(arr[i] >  0.0001 && arr[i] < -0.0001))
+        {
+            hmean = 0;
+            break;
+            return false;
+        }
+
         sum = sum + 1.0 / arr[i];
     }
 
-    assert((sum > 0.0001 || sum < -0.0001) && "The sum of the array elements is 0!");
-    return size/sum;
+    //assert((sum > 0.0001 || sum < -0.0001) && "The sum of the array elements is 0!");
+    if (!sum)
+    {
+        hmean = 0;
+        return false;
+    }
+    hmean = size/sum;
+    return true;
 }
 
 // Задание 7.2.2
 int main()
 {
-    float arr[] = {0.00011,2,3,4,5,6,7,8,9,0};
-    float result;
+    float arr[] = {1,2,3,4,5,6,7,8,9,0};
+    float hmean;
 
-    if (harmonic_mean(arr, 9, result))
-    std::cout << harmonic_mean(arr, 9) << "\n";
+    if (harmonic_mean(arr, 10, hmean))
+    {
+        std::cout << "Гармоническое среднее: " << hmean << "\n";
+    } else {
+        std::cout << "Возможно деление на ноль!" << "\n";
+    }
 
     return 0;
 }
