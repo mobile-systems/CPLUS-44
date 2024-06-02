07cebdf31f153201edec555aed1f15d7b99de48d
Alexander Ocheretin <mobidev@yandex.ru>
2024-06-02 17:08:38 +0300

    Исключение типа bad_cast означает, что операция динамического приведения типов при приведении ссылок не удалась.

---
+++ CPLUS-44-7.4.2/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <typeinfo> // заголовочный файл, где объявлен bad_cast
+using namespace std;
+
+struct Foo { virtual ~Foo() {} };
+struct Bar { virtual ~Bar() {} };
+ // Раздел 7.4.1
+int main()
+{
+    Bar b;
+    try
+    {
+        Foo& f = dynamic_cast<Foo&>(b);
+    }
+    catch(const bad_cast& e)
+    {
+        cout << e.what() << endl;
+    }
+    return 0;
+}
