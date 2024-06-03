fffc789695d64e84c58fe5d49e5c7a5d31735f15
Alexander Ocheretin <mobidev@yandex.ru>
2024-06-03 11:18:07 +0300

    Задание 7.4.3 Создание собственных исключений.

--- CPLUS-44-7.4.3/main.cpp
+++ CPLUS-44-7.4.3/main.cpp
@@ -2,42 +2,91 @@
 #include <exception>
 using namespace std;
 
-class BadDivision : public exception
+class BadHarmonicMean: public exception
 {
+//private:
+//    float _val;
 public:
-    virtual const char* what() const noexcept override
+    //BadHarmonicMean(float val) : _val(val) {}
+    virtual const char* what() const noexcept override   
     {
-        return "ERROR: Bad Division Arguments!";
+        return "Harmonic mean exception!";
     }
+    //void Show() { cout << "Harmonic mean exception " << _val << endl; }
 };
 
-
-
-int Divide(int a, int b)
+float harmonic_mean(float arr[], int size)
 {
-    if (b == 0)
+    if (size == 0)
     {
-        throw BadDivision();
+        //throw BadHarmonicMean(0.0);
+        throw BadHarmonicMean();
+    }
+    float sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == 0.0)
+        {
+            //throw BadHarmonicMean(arr[i]);
+            throw BadHarmonicMean();
+        }
+        sum = sum + 1.0 / arr[i];
     }
 
-    return a / b;
+    if (sum < 0.0001 && sum > -0.0001)
+    {
+        //throw BadHarmonicMean(sum);
+        throw BadHarmonicMean();
+    }
+    return size / sum;
 }
 
 int main()
 {
-    int a = 4;
-    int b = 0;
+    float array1[2] = { 0.0, 0.0 };
+    float array2[2] = { 1.5, -1.5 };
+    float array3[2] = { 2.3, 0.5 };
+    float hmean;
 
     try
     {
-        int result = Divide(a, b);
-        cout << "Division result: " << result << endl;
+        hmean = harmonic_mean(array1, 2);
+        cout << "hmean = " << hmean << endl;
     }
     catch (exception& e)
     {
-        cout << e.what();
+        cout << e.what() << endl;
     }
 
+    try
+    {
+        hmean = harmonic_mean(array2, 2);
+        cout << "hmean = " << hmean << endl;
+    }
+    catch (exception& e)
+    {
+        cout << e.what() << endl;
+    }
+
+    try
+    {
+        hmean = harmonic_mean(array2, 0);
+        cout << "hmean = " << hmean << endl;
+    }
+    catch (exception& e)
+    {
+        cout << e.what() << endl;
+    }
+
+    try
+    {
+        hmean = harmonic_mean(array3, 2);
+        cout << "hmean = " << hmean << endl;
+    }
+    catch (exception& e)
+    {
+        cout << e.what() << endl;
+    }
 
     return 0;
 }
