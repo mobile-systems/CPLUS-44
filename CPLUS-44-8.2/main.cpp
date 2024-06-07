0dd07918d5ff2a854bc8a461108fc5bd7c266ee6
Alexander Ocheretin <mobidev@yandex.ru>
2024-06-07 13:40:07 +0300

    8.2. Шаблоны. Синтаксис и компиляция. Синтаксис и компиляция шаблонов.

---
+++ CPLUS-44-8.2/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+template<typename T> T Add(T a, T b) {
+    T sum;
+    sum = a + b;
+    return sum;
+}
+ 
+int main()
+{
+    setlocale(LC_ALL, "");
+ 
+    int x = 3;
+    int y = 6;
+    cout << "Сложение целых чисел " << x << " и " << y << " равно " << Add<int>(x, y) << endl;
+ 
+    float a = 2.5;
+    float b = 3.7;
+    cout << "Сложение вещественных чисел " << a << " и " << b << " равно " << Add<float>(a, b) << endl;
+ 
+    cout << "Сложение строк " << "Hello " << " и " << "World!" << " равно " << Add<string>("Hello ", "World!") << endl;
+ 
+ 
+    return 0;
+}
