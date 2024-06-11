9c1a8a8cf2180092e037714bbad3bb2f7fb341b2
Alexander Ocheretin <mobidev@yandex.ru>
2024-06-11 09:06:35 +0300

    typeid — это оператор, который позволяет определить тип объекта во время выполнения.

--- CPLUS-44-8.6/main.cpp
+++ CPLUS-44-8.6/main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
-
+#include <typeinfo>
 using namespace std;
+
 template<typename T1, typename T2> auto Add(T1 a, T2 b) -> decltype(a + b) {
     return a + b;
 }
 
 int main() {
-    Add(2, 3);     // Тип возвращаемого значения будет int
-    Add(2, 3L);    // Тип возвращаемого значения будет long
-    Add(2U, 3);    // Тип возвращаемого значения будет unsigned int
-    Add(2.2f, 3);  // Тип возвращаемого значения будет float
+    cout << Add(2, 3) << endl;     // Тип возвращаемого значения будет int
+    cout << Add(2, 3L) << endl;    // Тип возвращаемого значения будет long
+    cout << Add(2U, 3) << endl;    // Тип возвращаемого значения будет unsigned int
+    cout << Add(2.2f, 3) << endl;  // Тип возвращаемого значения будет float
+
+    auto sum1 = Add(2, 3);              
+    cout << typeid(sum1).name() << endl;
+    auto sum2 = Add(2, 3L);            
+    cout << typeid(sum2).name() << endl;
+    auto sum3 = Add(2U, 3);             
+    cout << typeid(sum3).name() << endl;
+    auto sum4 = Add(2.2f, 3);           
+    cout << typeid(sum4).name() << endl;
+    
     return 0;
 }
