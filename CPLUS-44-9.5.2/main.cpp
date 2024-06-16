efdc617a959f6d31bec6abd9d00946cae82865e0
Alexander Ocheretin <mobidev@yandex.ru>
2024-06-16 18:13:08 +0300

    Правило трёх гласит, что если вы явно указывает в классе что-либо из троицы:
    конструктор копирования,
    оператор присваивания копированием,
    деструктор,
    То вам, скорее всего, необходимо явно указать всю троицу.

---
+++ CPLUS-44-9.5.2/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+class Int
+{
+public:
+    int* pi;
+    Int()
+    {
+        pi = new int(1);
+        cout << "Int new class:\t" << pi << endl;
+    }
+
+    Int(const Int& other)
+    {
+        pi = new int(*other.pi); //выдел€ем пам€ть под свой экземпл€р переменной и копируем туда значение, которое содержитс€ в переменной other
+        cout << "Int copy class:\t" << pi << endl;
+    }
+
+    Int& operator=(const Int& other)
+    {
+        pi = new int(*other.pi);//выдел€ем пам€ть под свой экземпл€р переменной и копируем туда значение, которое содержитс€ в переменной other
+        cout << "Int equa class:\t" << pi << endl;
+    }
+
+    ~Int()
+    {
+        delete pi;
+    }
+};
+
+
+int main()
+{
+    {
+        Int i;
+        Int j = i;
+        cout << "Sizeof class: " << sizeof(Int) << endl;
+    }
+
+
+    return 0;
+}
