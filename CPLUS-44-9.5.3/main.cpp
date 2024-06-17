29acf38079aaaf1bf65cbe34fe76b9e807b1dd52
Alexander Ocheretin <mobidev@yandex.ru>
2024-06-17 16:09:38 +0300

    Добавляем динамический массив.

--- CPLUS-44-9.5.3/main.cpp
+++ CPLUS-44-9.5.3/main.cpp
@@ -1,23 +1,36 @@
 ﻿#include <iostream>
 using namespace std;
 
-
 template <class T>
 class DynamicArray
 {
     T* _array;
     int _length;
 public:
-    T* pi;
+    //T* pi;
     DynamicArray()
     {
-        pi = new T(1);
-        cout << "Int new class:\t" << pi << endl;
+        //pi = new T(1);
+        _array = new T[1] {};
+        _length = 1;
+        cout << "Int new class:\t" << _length << endl;
+    }
+    DynamicArray(int lenght)
+    {
+        if (lenght <= 0)
+            throw bad_array_new_length();
+
+        if (lenght > 0) {
+            _array = new T[lenght] {};
+            _length = lenght;
+        }
+
+        cout << "Int new class:\t" << _lenght << endl;
     }
 
     DynamicArray(const DynamicArray& other)
     {
-        pi = new T(*other.pi); //выделяем память под свой экземпляр переменной и копируем туда значение, которое содержится в переменной other
+        _array = new T[_length](*other._array); //выделяем память под свой экземпляр переменной и копируем туда значение, которое содержится в переменной other
         cout << "Int copy class:\t" << pi << endl;
     }
 
@@ -27,9 +40,19 @@ public:
         cout << "Int equa class:\t" << pi << endl;
     }
 
+    DynamicArray& operator[](const T& other)
+    {
+        pi = new T(*other.pi);//выделяем память под свой экземпляр переменной и копируем туда значение, которое содержится в переменной other
+        
+        cout << "Int equa class:\t" << pi << endl;
+    }
+
     ~DynamicArray()
     {
         delete pi;
+    }
+    T getLength() {
+        return _length;
     }
 };
 
