8525410f4749d16622dec6d7743630ca4879c288
Alexander Ocheretin <mobidev@yandex.ru>
2024-06-18 15:09:23 +0300

    Задание 9.5.3 Напишите шаблонный класс

--- CPLUS-44-9.5.3/main.cpp
+++ CPLUS-44-9.5.3/main.cpp
@@ -1,55 +1,72 @@
 ﻿#include <iostream>
 using namespace std;
 
+class bad_range final : public exception {
+public:
+    virtual const char* what() const noexcept override { return "Index out of range!"; }
+};
+
+class bad_length final : public exception {
+public:
+    virtual const char* what() const noexcept override { return "Lenght error!"; }
+};
+
 template <class T>
 class DynamicArray
 {
     T* _array;
     int _length;
 public:
-    //T* pi;
-    DynamicArray()
-    {
-        //pi = new T(1);
-        _array = new T[1] {};
-        _length = 1;
-        cout << "Int new class:\t" << _length << endl;
-    }
     DynamicArray(int lenght)
     {
         if (lenght <= 0)
-            throw bad_array_new_length();
+            throw bad_length();
 
         if (lenght > 0) {
             _array = new T[lenght] {};
             _length = lenght;
         }
 
-        cout << "Int new class:\t" << _lenght << endl;
+        cout << "Int new class:\t" << _length << endl;
     }
 
-    DynamicArray(const DynamicArray& other)
+    DynamicArray(const DynamicArray &other)
     {
-        _array = new T[_length](*other._array); //выделяем память под свой экземпляр переменной и копируем туда значение, которое содержится в переменной other
-        cout << "Int copy class:\t" << pi << endl;
+        _length = other._length;
+        delete[] _array;
+        _array = new T[_length];
+        for (int i = 0; i < _length; ++i)
+        {
+            _array[i] = other._array[i];
+        }
+    }
+    DynamicArray& operator=(const DynamicArray &other)
+    {
+        _length = other._length;
+        delete[] _array;
+        _array = new T[_length];
+        for (int i = 0; i < _length; ++i)
+        {
+            _array[i] = other._array[i];
+        }
+
+        return *this;
     }
 
-    DynamicArray& operator=(const T& other)
+    T& operator[](const int index)
     {
-        pi = new T(*other.pi);//выделяем память под свой экземпляр переменной и копируем туда значение, которое содержится в переменной other
-        cout << "Int equa class:\t" << pi << endl;
-    }
-
-    DynamicArray& operator[](const T& other)
-    {
-        pi = new T(*other.pi);//выделяем память под свой экземпляр переменной и копируем туда значение, которое содержится в переменной other
-        
-        cout << "Int equa class:\t" << pi << endl;
+        if (index >= 0 && index < _length) {}
+        else
+        {
+            // "Index out of range";
+            throw bad_range();
+        }
+        return _array[index];
     }
 
     ~DynamicArray()
     {
-        delete pi;
+        delete[] _array;
     }
     T getLength() {
         return _length;
@@ -60,10 +77,23 @@ public:
 int main()
 {
     {
-        DynamicArray <int>i;
-        DynamicArray <long>j;
+        int len = 50;
+        DynamicArray <int>i(len);
+        for (int x=0;x<len;x++){
+            i[x] = x;
+            cout << " x: " << x;
+            cout << " i: " << i[x] ;
+        }
+        cout << endl;
+        //DynamicArray <long>j;
         cout << "Sizeof class: " << sizeof(i) << endl;
-        cout << "Sizeof class: " << sizeof(j) << endl;
+        cout << "State class i: " << i[0] << endl;
+        cout << "State class i: " << i[20] << endl;
+        cout << "State class i: " << i[21] << endl;
+        //i[22]=22;i[23]=23;
+        cout << "State class i: " << i[22] << endl;
+        //cout << "Sizeof class: " << sizeof(j) << endl;
+        //cout << "State class j: " << j[21] << endl;
     }
 
 
