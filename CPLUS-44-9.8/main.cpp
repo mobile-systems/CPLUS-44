2ac1f59bda488bc148355c02978d1c684881bd37
Alexander Ocheretin <mobidev@yandex.ru>
2024-06-19 15:57:22 +0300

    Тест bar() = 9;

--- CPLUS-44-9.8/main.cpp
+++ CPLUS-44-9.8/main.cpp
@@ -7,6 +7,17 @@ void swap(mstring& str1, mstring& str2)
     str2 = temp;
 }
 
+int foo()
+{
+    return 42;
+}
+
+int& bar()
+{
+    static int i = 0;
+    return i;
+}
+
 int main()
 {
     mstring string1("string1");
@@ -14,5 +25,12 @@ int main()
     swap(string1, string2);
     
 
+int a = foo(); // foo возвращает rvalue, можем присвоить значение переменной a
+cout << a << " " << foo() << "\n" << bar() << endl;
+
+bar() = 9; // bar() возвращает lvalue, можем присвоить значение
+cout << bar() << endl;
+
+//foo() = 9; // ошибка foo() возвращает rvalue
     return 0;
 }
