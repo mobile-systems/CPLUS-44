a6aab35b4073308ccf6a273228bbc544b211ce30
Alexander Ocheretin <mobidev@yandex.ru>
2024-09-30 13:57:28 +0300

    Настройка конфигурации cmake

--- curl/main.cpp
+++ curl/main.cpp
@@ -9,7 +9,8 @@ int main()
     if(curl_handle)
     {
         // задаем  url адрес
-        curl_easy_setopt(curl_handle, CURLOPT_URL, "https://www.cyberforum.ru");
+        //curl_easy_setopt(curl_handle, CURLOPT_URL, "https://www.moex.ru");
+        curl_easy_setopt(curl_handle, CURLOPT_URL, "https://iss.moex.com/iss/apps/infogrid/stock/rates.json?_=1727681260480&lang=ru&iss.meta=off&sort_order=asc&sort_column=SECID&start=0&limit=100&bg=stock_tplus");
         curl_easy_setopt(curl_handle, CURLOPT_USERAGENT,"Mozilla/5.0 (Windows NT 6.1; rv:16.0) Gecko/20100101 Firefox/16.0");
 
         // выполняем запрос
