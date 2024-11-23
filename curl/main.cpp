ac1f4ad36c9c9668445ebf4a490ee666c0f11caf
Alexander Ocheretin <mobidev@yandex.ru>
2024-11-23 21:05:20 +0300

    Change URL to MOEX

--- curl/main.cpp
+++ curl/main.cpp
@@ -27,7 +27,7 @@ int main(int argc, char* argv[])
     if(curl_handle)
     {
         /* Set the username and password */
-        cout << "Login: " << moexLogin << "Password: " << moexPassword << endl;
+        cout << "Login: " << moexLogin << " Password: " << moexPassword << endl;
         curl_easy_setopt(curl_handle, CURLOPT_USERNAME, argv[1]); //moexLogin);
         curl_easy_setopt(curl_handle, CURLOPT_PASSWORD, argv[2]); //moexPassword);
         curl_easy_setopt(curl_handle, CURLOPT_URL, "https://passport.moex.com/authenticate");
@@ -63,21 +63,13 @@ int main(int argc, char* argv[])
             curl_slist_free_all(cookies);
         }
 
- 
-        /* Set the authorization identity (identity to act as) */
-        //curl_easy_setopt(curl_handle, CURLOPT_SASL_AUTHZID, "shared-mailbox");
- 
-        /* Force PLAIN authentication */
-        //curl_easy_setopt(curl_handle, CURLOPT_LOGIN_OPTIONS, "AUTH=PLAIN");
-        // задаем  url адрес
-        //curl_easy_setopt(curl_handle, CURLOPT_URL, "https://www.moex.ru");
+        // задаем url адрес
         //curl_easy_setopt(curl_handle, CURLOPT_URL, "https://iss.moex.com/iss/apps/infogrid/stock/rates.json?_=1727681260480&lang=ru&iss.meta=off&sort_order=asc&sort_column=SECID&start=0&limit=100&bg=stock_tplus");
-        curl_easy_setopt(curl_handle, CURLOPT_URL, "https://iss.moex.com/iss/analyticalproducts/futoi/securities/si.json?from=2024-11-18&till=2024-11-19");
-        //curl_easy_setopt(curl_handle, CURLOPT_URL, "https://iss.moex.com/iss/analyticalproducts/futoi/securities");
-        //curl_easy_setopt(curl_handle, CURLOPT_URL, "https://iss.moex.com/iss/analyticalproducts/futoi/securities");
+        //curl_easy_setopt(curl_handle, CURLOPT_URL, "https://iss.moex.com/iss/analyticalproducts/futoi/securities/si.json?from=2024-11-18&till=2024-11-19");
+        curl_easy_setopt(curl_handle, CURLOPT_URL, "https://iss.moex.com/iss/analyticalproducts/futoi/securities");
         //curl_easy_setopt(curl_handle, CURLOPT_USERAGENT,"Mozilla/5.0 (Windows NT 6.1; rv:16.0) Gecko/20100101 Firefox/16.0");
         //curl_easy_setopt(curl_handle, CURLOPT_USERAGENT,"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0"); // Firefox Kubuntu 24.04
-        curl_easy_setopt(curl_handle, CURLOPT_USERAGENT,"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 YaBrowser/24.10.0.0 Safari/537.36"); // Yandex Browser Kubuntu 24.04
+        //curl_easy_setopt(curl_handle, CURLOPT_USERAGENT,"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 YaBrowser/24.10.0.0 Safari/537.36"); // Yandex Browser Kubuntu 24.04
 
         // выполняем запрос
         /*CURLcode*/ res = curl_easy_perform(curl_handle);
