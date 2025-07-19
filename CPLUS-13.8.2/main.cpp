e0b08fceab7f1e54d33ce622ea585da70ec1374e
Alexander Ocheretin <mobidev@yandex.ru>
2025-07-19 23:40:05 +0300

    Приводим бинарное дерево в соответствие примеру (13.8.2)

--- CPLUS-13.8.2/main.cpp
+++ CPLUS-13.8.2/main.cpp
@@ -8,15 +8,19 @@ int main()
     BinaryTree tree;
     
     //tree.setRoot(2); // задали корень
-    tree.addInt(4); // добавляем узлы
-    tree.addInt(0);
-    tree.addInt(3);
-    tree.addInt(1);
-    tree.addInt(5);
-    tree.addInt(6);
+    tree.addInt(5); // добавляем узлы
+    tree.addInt(4);
+    tree.addInt(-17);
+    tree.addInt(20);
+    tree.addInt(13);
+    tree.addInt(43);
+    tree.addInt(11);
+    tree.addInt(18);
+    tree.addInt(15);
+    tree.addInt(19);
     std::cout << "Init print" << std::endl;
     //tree.print();
-    tree.delInt(4); // удалили узел 1
+    tree.delInt(20); // удалили узел 1
     std::cout << "After del" << std::endl;
     //tree.print();
     return 0;
